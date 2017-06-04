9c14e55f978bb299efd8256f44f60c9c0a1799aa
Carl Hamilton <carlanthonyhamilton@gmail.com>
2017-06-04 13:35:22 +1000

    Fix for tests

--- generate.js
+++ generate.js
@@ -3,7 +3,7 @@
 module.exports = function (order) {
   let config = []
 
-  for (let property in order) {
+  for (const property in order) {
     if (order.hasOwnProperty(property)) {
       for (let i = 0; i < order[property].length; i++) {
         config = config.concat(order[property][i])
