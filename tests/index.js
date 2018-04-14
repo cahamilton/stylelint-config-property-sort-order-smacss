64c89dca200ede3f0b4f515f27dae068c4c78156
Carl Hamilton <carlanthonyhamilton@gmail.com>
2018-04-14 15:11:32 +1000

    Resolve new ESLint error (sort-requires)

--- tests/index.js
+++ tests/index.js
@@ -1,12 +1,12 @@
 "use strict"
 
-const test = require("tape")
 const stylelint = require("stylelint")
-const stylelintConfig = require("..")
-const order = require("./sort-order")
-const expected = require("./expected")
+const test = require("tape")
 
+const config = require("..")
+const expected = require("./expected")
 const generate = require("../generate")
+const order = require("./sort-order")
 
 test("Generate configuration", function (t) {
   const actual = generate(order)
@@ -17,7 +17,7 @@ test("Generate configuration", function (t) {
 test("Stylelint configuration", function (t) {
   stylelint.lint({
     code: "a { color: red; top: 0; }",
-    config: stylelintConfig,
+    config,
   }).then(function (output) {
     const actual = output.results[0].warnings[0].text
     t.equal(actual, "Expected \"top\" to come before \"color\" (order/properties-order)")
