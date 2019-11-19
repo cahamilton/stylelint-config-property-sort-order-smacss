271b5b93a5a9bdda13e7a84919356f88abb4acac
Carl Hamilton <carlanthonyhamilton@gmail.com>
2019-11-19 11:53:27 +1100

    Update ESLint package

--- generate.js
+++ generate.js
@@ -4,7 +4,7 @@ module.exports = function (order) {
   let config = []
 
   for (const property in order) {
-    if (order.hasOwnProperty(property)) {
+    if (Object.prototype.hasOwnProperty.call(order, property)) {
       for (let i = 0; i < order[property].length; i++) {
         config = config.concat(order[property][i])
       }
